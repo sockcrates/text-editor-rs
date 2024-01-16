bc7ce6efdf16f2e7dd34cd4f9a26a106b77c7b6f
markg <17348049+sockcrates@users.noreply.github.com>
2024-01-16 16:33:56 -0500

    Read from stdin in cookedmode; quit on "q" press

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+        match std::io::stdin().read_line(&mut input) {
+            Ok(_) => {},
+            Err(e) => println!("Error: {}", e),
+        }
+
+        match input.trim() {
+            "q" => std::process::exit(0),
+            _ => println!("{}", input.trim()),
+        }
+    }
 }
