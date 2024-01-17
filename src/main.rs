dc0d50c0b9ab5ba258c055d4ffa1f48fc0849b8a
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2024-01-16 21:27:40 -0500

    Handle all errors

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use termios::{Termios, tcgetattr, tcsetattr};
+use termios::{tcgetattr, tcsetattr, Termios};
 
 fn enable_raw_mode() {
-    let mut termios = Termios::from_fd(0).unwrap();
+    let mut termios = Termios::from_fd(0).unwrap_or_else(|e| {
+        println!("Error: {}", e);
+        std::process::exit(1);
+    });
     match tcgetattr(0, &mut termios) {
-        Ok(_) => {},
-        Err(e) => println!("Error: {}", e),
+        Ok(_) => {}
+        Err(e) => {
+            println!("Error: {}", e);
+            std::process::exit(1);
+        }
     }
     termios.c_lflag &= !(termios::ECHO);
     match tcsetattr(0, termios::TCSAFLUSH, &termios) {
-        Ok(_) => {},
-        Err(e) => println!("Error: {}", e),
+        Ok(_) => {}
+        Err(e) => {
+            println!("Error: {}", e);
+            std::process::exit(1);
+        }
     }
 }
 
@@ -18,7 +27,7 @@ fn main() {
     loop {
         let mut input = String::new();
         match std::io::stdin().read_line(&mut input) {
-            Ok(_) => {},
+            Ok(_) => {}
             Err(e) => println!("Error: {}", e),
         }
 
