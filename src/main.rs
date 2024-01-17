23fa653f3bb9e8f50742f9825724c152349bc2a7
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2024-01-16 21:12:26 -0500

    Add termios; remove echoing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use termios::{Termios, tcgetattr, tcsetattr};
+
+fn enable_raw_mode() {
+    let mut termios = Termios::from_fd(0).unwrap();
+    tcgetattr(0, &mut termios);
+    termios.c_lflag &= !(termios::ECHO);
+    tcsetattr(0, termios::TCSAFLUSH, &termios);
+}
+
 fn main() {
+    enable_raw_mode();
     loop {
         let mut input = String::new();
         match std::io::stdin().read_line(&mut input) {
