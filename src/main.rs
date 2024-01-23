004345fd5f76dadf646e2ecd2786db104cdc7dd7
markg <17348049+sockcrates@users.noreply.github.com>
2024-01-22 22:51:15 -0500

    Add functions to impl editor

--- src/main.rs
+++ src/main.rs
@@ -10,45 +10,47 @@ struct Editor {
     raw_termios: Termios,
 }
 
-fn exit_with_error(location: &str, err: &dyn Error) {
-    println!("Error in {}: {}", location, err);
-    exit(1);
-}
-
-fn read_key(stdin: &mut Stdin) -> u8 {
-    let mut input: [u8; 1] = [0; 1];
-    match stdin.read(&mut input) {
-        Ok(_) => {}
-        Err(e) => {
-            exit_with_error("reading stdin", &e);
-        }
-    };
-    input[0]
-}
-
-fn process_keypress(key: u8, original_termios: &mut Termios) {
-    match key {
-        b'\x11' => {
-            refresh_screen();
-            terminal::disable_raw_mode(original_termios);
-            exit(0);
-        }
-        b'\r' => print!("\r\n"),
-        _ => print!("{}", key as char),
+impl Editor {
+    fn exit_with_error(location: &str, err: &dyn Error) {
+        println!("Error in {}: {}", location, err);
+        exit(1);
     }
-}
 
-fn draw_rows() {
-    for _ in 0..24 {
-        print!("~\r\n");
+    fn read_key(stdin: &mut Stdin) -> u8 {
+        let mut input: [u8; 1] = [0; 1];
+        match stdin.read(&mut input) {
+            Ok(_) => {}
+            Err(e) => {
+                Self::exit_with_error("reading stdin", &e);
+            }
+        };
+        input[0]
     }
-}
 
-fn refresh_screen() {
-    print!("\x1b[2J");
-    print!("\x1b[H");
-    draw_rows();
-    print!("\x1b[H");
+    fn process_keypress(key: u8, original_termios: &mut Termios) {
+        match key {
+            b'\x11' => {
+                Self::refresh_screen();
+                terminal::disable_raw_mode(original_termios);
+                exit(0);
+            }
+            b'\r' => print!("\r\n"),
+            _ => print!("{}", key as char),
+        }
+    }
+
+    fn draw_rows() {
+        for _ in 0..24 {
+            print!("~\r\n");
+        }
+    }
+
+    fn refresh_screen() {
+        print!("\x1b[2J");
+        print!("\x1b[H");
+        Self::draw_rows();
+        print!("\x1b[H");
+    }
 }
 
 fn main() {
@@ -60,14 +62,14 @@ fn main() {
         raw_termios: original_termios.clone(),
     };
     terminal::enable_raw_mode(&mut editor.raw_termios);
-    refresh_screen();
+    Editor::refresh_screen();
     loop {
         let mut stdin = stdin();
         let stdout = stdout();
         stdout.lock().flush().unwrap_or_else(|e| {
-            exit_with_error("flushing stdout", &e);
+            Editor::exit_with_error("flushing stdout", &e);
         });
-        let key = read_key(&mut stdin);
-        process_keypress(key, &mut original_termios);
+        let key = Editor::read_key(&mut stdin);
+        Editor::process_keypress(key, &mut original_termios);
     }
 }
