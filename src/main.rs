208bad97ceadf710e502cea86e7134f6cbe397f3
markg <17348049+sockcrates@users.noreply.github.com>
2024-12-18 00:01:11 -0500

    Implement reading from file where path is provided by the first arg

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,11 @@ use std::io::Error;
 use editor::Editor;
 
 fn main() -> Result<(), Error> {
-    Editor::try_new()?.run()
+    let args = std::env::args().collect::<Vec<String>>();
+    let file_name = if args.len() >= 2 {
+        Some(args[1].as_str())
+    } else {
+        None
+    };
+    Editor::try_new()?.run(file_name)
 }
