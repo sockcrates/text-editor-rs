043967393a7678c322806f78f49c491160d4ed8c
markg <17348049+sockcrates@users.noreply.github.com>
2024-12-17 22:55:38 -0500

    Debug config dir -> .gitignore

--- .gdbinit
+++ .gdbinit
@@ -1,1 +1,3 @@
 file ./target/debug/text-editor-rs
+break ./src/editor/mod.rs:45
+set args ~/Documents/test.txt
--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -1,5 +1,10 @@
 mod append_buffer;
-use std::{io::Error, mem::take, process::exit};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Error},
+    mem::take,
+    process::exit,
+};
 
 use append_buffer::AppendBuffer;
 
@@ -219,13 +224,17 @@ impl Editor {
         Ok(editor)
     }
 
-    fn open(&mut self) -> () {
-        self.line = String::from("Hello, world!");
-        self.num_rows = 1;
+    fn open(&mut self, file_name: &str) -> Result<(), Error> {
+        let file = File::open(file_name)?;
+        let mut reader = BufReader::new(file);
+        reader.read_line(&mut self.line)?;
+        Ok(self.num_rows = 1)
     }
 
-    pub fn run(&mut self) -> Result<(), Error> {
-        self.open();
+    pub fn run(&mut self, file_name: Option<&str>) -> Result<(), Error> {
+        if let Some(file_to_open) = file_name {
+            self.open(file_to_open)?;
+        }
         loop {
             self.refresh_screen()?;
             self.process_keypress()?;
